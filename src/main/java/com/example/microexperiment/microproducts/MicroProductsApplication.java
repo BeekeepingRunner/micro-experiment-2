260fab81cea261fb2f50fff823d65dfdbd04a89d
Bartosz Biegajło <bartosz.biegajlo@gmail.com>
2023-12-22 20:54:09 +0100

    project init

---
+++ src/main/java/com/example/microexperiment/microproducts/MicroProductsApplication.java
@@ -0,0 +1,13 @@
+package com.example.microexperiment.microproducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroProductsApplication.class, args);
+	}
+
+}
